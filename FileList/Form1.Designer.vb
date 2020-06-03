c6d1a524bbb56eaf2981d762a47b43cb63b29488
yosshii50 <24950665+yosshii50@users.noreply.github.com>
2020-06-03 11:53:08 +0900

    Add files via upload

---
+++ FileList/Form1.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ファイルFToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.追加AToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.フォルダ追加DToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.終了ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.全消去NToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ファイルFToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(671, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ファイルFToolStripMenuItem
+        '
+        Me.ファイルFToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.追加AToolStripMenuItem, Me.フォルダ追加DToolStripMenuItem, Me.ToolStripMenuItem2, Me.全消去NToolStripMenuItem, Me.ToolStripMenuItem1, Me.終了ToolStripMenuItem})
+        Me.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem"
+        Me.ファイルFToolStripMenuItem.Size = New System.Drawing.Size(67, 20)
+        Me.ファイルFToolStripMenuItem.Text = "ファイル(&F)"
+        '
+        '追加AToolStripMenuItem
+        '
+        Me.追加AToolStripMenuItem.Name = "追加AToolStripMenuItem"
+        Me.追加AToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.追加AToolStripMenuItem.Text = "ファイル追加(&F)"
+        '
+        'フォルダ追加DToolStripMenuItem
+        '
+        Me.フォルダ追加DToolStripMenuItem.Name = "フォルダ追加DToolStripMenuItem"
+        Me.フォルダ追加DToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.フォルダ追加DToolStripMenuItem.Text = "フォルダ追加(&D)"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(149, 6)
+        '
+        '終了ToolStripMenuItem
+        '
+        Me.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem"
+        Me.終了ToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.終了ToolStripMenuItem.Text = "終了(&X)"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(0, 24)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(671, 450)
+        Me.ListBox1.TabIndex = 1
+        '
+        '全消去NToolStripMenuItem
+        '
+        Me.全消去NToolStripMenuItem.Name = "全消去NToolStripMenuItem"
+        Me.全消去NToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.全消去NToolStripMenuItem.Text = "全消去(&N)"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(149, 6)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(671, 474)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "FileList"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ファイルFToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 終了ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 追加AToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents フォルダ追加DToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ToolStripMenuItem2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents 全消去NToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ FileList/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+
+    Private Sub 追加AToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles 追加AToolStripMenuItem.Click
+
+        Dim ofd As New OpenFileDialog()
+
+        ofd.Filter = "すべてのファイル(*.*)|*.*"
+        ofd.Multiselect = True
+        ofd.CheckFileExists = False
+        ofd.CheckPathExists = False
+
+        If ofd.ShowDialog() = DialogResult.OK Then
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+
+            For Each WrkStr As String In ofd.FileNames
+                ListBox1.Items.Add(WrkStr)
+            Next
+
+        End If
+
+    End Sub
+
+    Private Sub フォルダ追加DToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles フォルダ追加DToolStripMenuItem.Click
+
+        Dim fbd As New FolderBrowserDialog
+        If fbd.ShowDialog(Me) = DialogResult.OK Then
+            '選択されたフォルダを表示する
+            ListBox1.Items.Add(fbd.SelectedPath & "\")
+        End If
+
+    End Sub
+
+    Private Sub 終了ToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles 終了ToolStripMenuItem.Click
+        Me.Close()
+        Me.Dispose()
+    End Sub
+
+    Private Sub 全消去NToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles 全消去NToolStripMenuItem.Click
+        ListBox1.Items.Clear()
+    End Sub
+End Class
